af2f8ad7996d7328074426072a33f31bd2697d55
CKdave <chirag.django@gmail.com>
2020-08-28 00:47:13 +0530

    tweaked for heroku/added gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn taskmaster.wsgi
--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
@@ -124,3 +125,4 @@ LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = 'login'
 
 django_heroku.settings(locals())
+
